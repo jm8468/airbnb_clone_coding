2a5f9e3b179aa351ee81129734fd41930c445000
jm8468 <jmtoyplay1113@gmail.com>
2024-03-27 23:31:15 +0900

    Feat: recommand tour list tab panel

--- clone.js
+++ clone.js
@@ -3,23 +3,22 @@ const panelsSeo = document.querySelectorAll('#panel-seo div');
 
 function tabSeoButton(event) {
     const button = event.target;
-
     // 선택한 aria-selected만 true가 되게 하는 기능
     tabsSeo.forEach(tab => {
         tab.setAttribute("aria-selected", "false");
     });
     button.setAttribute("aria-selected", "true");
-    console.log(button);
     // aria-selected가 true인 버튼만 검은색 폰트 적용하는 기능
-    tabsSeo.forEach(tab => {
+    tabsSeo.forEach((tab, index) => {
         if(tab !== button) {
             tab.classList.remove("font-black");
             tab.classList.add("font-grey");
-            
+            panelsSeo[index].setAttribute("hidden", true);
         }
         else {
             tab.classList.remove("font-grey");
             tab.classList.add("font-black");
+            panelsSeo[index].removeAttribute("hidden");
         }
     });
     
