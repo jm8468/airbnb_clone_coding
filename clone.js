608ce487edfaa18c71996f7145cd1089e2246e3c
jm8468 <jmtoyplay1113@gmail.com>
2024-04-09 17:52:17 +0900

    Feat: normalize highlighting selected button except for others

--- clone.js
+++ clone.js
@@ -1,9 +1,12 @@
-const tabsSeo = document.querySelectorAll('#tab-seo button');
-const mapImgPressEffects = document.querySelectorAll('.press-effect')
+const tabsSeo = document.querySelectorAll('#tab-seo > button');
+const tabsPlacesByAreasTabId = document.querySelectorAll('#tab-placesByAreasTabId > button')
 
-function tabSeoButton(event) {
-    const panelsSeo = document.querySelectorAll('#panel-seo div');
+function tabButton(event) {
     const button = event.target;
+    const parentButton = button.parentElement.parentElement;
+    const panels = parentButton.querySelectorAll('div[id^="panel"] > div');
+    console.log(panels);
+    
     // 선택한 aria-selected만 true가 되게 하는 기능
     tabsSeo.forEach(tab => {
         tab.setAttribute("aria-selected", "false");
@@ -16,37 +19,21 @@ function tabSeoButton(event) {
             tab.classList.remove("font-black");
             tab.classList.remove("selected-button-highlight");
             tab.classList.add("font-grey");
-            panelsSeo[index].setAttribute("hidden", true);
+            panels[index].setAttribute("hidden", true);
         }
         else {
             tab.classList.remove("font-grey");
             tab.classList.add("font-black");
             tab.classList.add("selected-button-highlight");
-            panelsSeo[index].removeAttribute("hidden");
+            panels[index].removeAttribute("hidden");
         }
     });
 
 }
 
-// 마우스 이벤트에 따라 세계지도 크기를 바꾸는 애니메이션 효과
-function addMapImgEffect(event) {
-    event = event.currentTarget;
-    event.classList.add('scale-90percent');
-}
-function removeMapImgEffect(event) {
-    event = event.currentTarget;
-    event.classList.remove('scale-90percent');
-}
-
-tabsSeo.forEach(tabSeo => {
-    tabSeo.addEventListener("click", tabSeoButton);
+tabsSeo.forEach(tab => {
+    tab.addEventListener("click", tabButton)
 });
-mapImgPressEffects.forEach(effect => {
-    effect.addEventListener("mousedown", addMapImgEffect);
-});
-mapImgPressEffects.forEach(effect => {
-    effect.addEventListener("mouseup", removeMapImgEffect);
-});
-mapImgPressEffects.forEach(effect => {
-    effect.addEventListener("mouseleave", removeMapImgEffect);
+tabsPlacesByAreasTabId.forEach(tab => {
+    tab.addEventListener("click", tabButton)
 });
