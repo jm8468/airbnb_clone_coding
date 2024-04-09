ea9ad1f512f0d239a94ae7f9697af076aff13db4
jm8468 <jmtoyplay1113@gmail.com>
2024-04-09 20:58:13 +0900

    Feat: normalize highlighting selected button except for others

--- clone.js
+++ clone.js
@@ -1,20 +1,19 @@
-const tabsSeo = document.querySelectorAll('#tab-seo > button');
-const tabsPlacesByAreasTabId = document.querySelectorAll('#tab-placesByAreasTabId > button')
+const tabsSeo = document.querySelectorAll('#tabs-seo > button');
+const tabsPlacesByAreasTabId = document.querySelectorAll('#tabs-placesByAreasTabId > button')
 
 function tabButton(event) {
     const button = event.target;
     const parentButton = button.parentElement.parentElement;
-    const panels = parentButton.querySelectorAll('div[id^="panel"] > div');
-    console.log(panels);
-    
+    const buttons = parentButton.querySelectorAll('div[id^="tabs-"] > button[id^="tab-"]')
+    const panels = parentButton.querySelectorAll('div[id^="panels-"] > div[id^="panel-"]');
     // 선택한 aria-selected만 true가 되게 하는 기능
-    tabsSeo.forEach(tab => {
+    buttons.forEach(tab => {
         tab.setAttribute("aria-selected", "false");
     });
     button.setAttribute("aria-selected", "true");
 
     // aria-selected가 true인 버튼만 강조하는 기능
-    tabsSeo.forEach((tab, index) => {
+    buttons.forEach((tab, index) => {
         if (tab.getAttribute("aria-selected") === "false") {
             tab.classList.remove("font-black");
             tab.classList.remove("selected-button-highlight");
